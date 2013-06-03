3d5ea3e118e4935f3534ed2abf36363128267ca2
Jeremie Graulle <jeremiex.graulle@intel.com>
2013-06-03 17:26:25 +0200

    audioCommsUtils: add host compilation for full_rw
    BZ: 110392
    
    The librairy full_rw is not compiled for host.
    
    Add host compilation for full_rw in Android.mk
    
    Change-Id: I258bf369d3c00927eb213f52741ace8f8c959d50
    Signed-off-by: Jeremie Graulle <jeremiex.graulle@intel.com>
    Reviewed-on: http://android.intel.com:8080/109984
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Soulard, Mathieu <mathieu.soulard@intel.com>
    Reviewed-by: Gonzalve, Sebastien <sebastien.gonzalve@intel.com>
    Tested-by: Dixon, CharlesX <charlesx.dixon@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- full_rw/Android.mk
+++ full_rw/Android.mk
@@ -1,23 +1,23 @@
 LOCAL_PATH:= $(call my-dir)
 
+# Build for target
 include $(CLEAR_VARS)
-
 LOCAL_COPY_HEADERS_TO := full_rw
-LOCAL_COPY_HEADERS := \
-    full_rw.h
-include $(BUILD_COPY_HEADERS)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := \
-    full_rw.c
-
+LOCAL_COPY_HEADERS :=  full_rw.h
+LOCAL_SRC_FILES := full_rw.c
 LOCAL_MODULE := libfull_rw
-
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_CFLAGS += -Werror -Wall
-
-LOCAL_C_INCLUDES += \
-    bionic
-
+LOCAL_C_INCLUDES += bionic
 include $(BUILD_STATIC_LIBRARY)
+
+# Build for host
+include $(CLEAR_VARS)
+LOCAL_COPY_HEADERS_TO := full_rw
+LOCAL_COPY_HEADERS :=  full_rw.h
+LOCAL_SRC_FILES := full_rw.c
+LOCAL_MODULE := libfull_rw_host
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Werror -Wall
+LOCAL_C_INCLUDES += bionic
+include $(BUILD_HOST_STATIC_LIBRARY)
