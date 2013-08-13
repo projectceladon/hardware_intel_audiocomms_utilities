bdd232c2a3a812d6f90f29fa1f0a971c7d5ea8de
Sebastien Gonzalve <sebastien.gonzalve@intel.com>
2013-08-13 17:48:35 +0200

    Utilities: Add assert macros
    BZ: 130529
    
    Asserts allow to enforce functions API and ease debugging, but for now, there
    is no standard way to perform asserts.
    
    This patch adds macros of asserts
    
    Change-Id: Iad6660f641821ca42800f09f4b592154adf99e5c
    Signed-off-by: Sebastien Gonzalve <sebastien.gonzalve@intel.com>
    Reviewed-on: http://android.intel.com:8080/125296
    Reviewed-by: Rocard, KevinX <kevinx.rocard@intel.com>
    Reviewed-by: De Chivre, Renaud <renaud.de.chivre@intel.com>
    Reviewed-by: Graulle, JeremieX <jeremiex.graulle@intel.com>
    Tested-by: Dixon, CharlesX <charlesx.dixon@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- include/AudioCommsAssert.h
+++ include/AudioCommsAssert.h
@@ -21,6 +21,11 @@
 #ifndef AUDIOCOMMS_ASSERT_H
 #define AUDIOCOMMS_ASSERT_H
 
+/**
+ * Macro to wrap gcc builtin function to specify that a condition is unlikely
+ * to happen. */
+#define audio_comms_unlikely(cond) __builtin_expect((cond), 0)
+
 #ifdef __cplusplus
 
 /**
@@ -58,6 +63,24 @@ class compileTimeAssertFailure<false>
 
 } // namespace audio_comms
 
+#include <iostream>
+/**
+ * Checks a condition and abort if it is not met.
+ * This assert is ALWAYS activated (even in release mode), thus it must be used
+ * wisely.
+ *
+ * @param[in] cond   the condition to check
+ * @param[in] iostr  an iostream giving some details about the error
+ */
+#define AUDIOCOMMS_ASSERT(cond, iostr) \
+ do { \
+   if (audio_comms_unlikely(!(cond))) { \
+       std::cerr << __BASE_FILE__ ":" << __LINE__ << ": Assertion " #cond " failed: " \
+                 << iostr << std::endl; \
+       abort(); \
+   } \
+ } while (false)
+
 #else // __cplusplus
 
 /**
@@ -76,8 +99,59 @@ class compileTimeAssertFailure<false>
         ; \
     }
 
-#endif // __cplusplus
+/**
+ * The macro AUDIO_COMMS_MKSTR(a) makes a string from an integer value.
+ * For example, the AUDIO_COMMS_MKSTR(11) will be replaced by "11".
+ *
+ * The important point here is that we need 2 macros to do this job properly on
+ * constants given by #define.
+ * For example if we have #define AUDIO 11 , the
+ * AUDIO_COMMS_MK_ESCAPED_STR(AUDIO) will be replaced by \"AUDIO\" and _NOT_
+ * "AUDIO". The difference is that calling it in
+ * printf(AUDIO_COMMS_MK_ESCAPED_STR(AUDIO)); will be replaced by
+ * printf(\"AUDIO\"); and that is obviously not correct (\"AUDIO\" is a string,
+ * but a string must be surrounded by "", and that is not the case.
+ *
+ * So the aim of calling this macro through another one is that the second
+ * macro will interpret \" as " and give the good result "AUDIO". So even if
+ * this is quite strange these two macros must be kept.
+ *
+ * This macro is used for example in the following situation :
+ * #define AUDIO 11
+ * char toto[AUDIO+1];
+ * sscanf(str, "%s", toto); <- does not specify the max size of the string
+ * sscanf(str, "%"AUDIO_COMMS_MKSTR(AUDIO)"s", toto); does the trick because
+ * will be replaced by sscanf(str, "%11s", toto);
+ * or to make string of filename:line number:
+ *   __BASE_FILE__ ":" audio_comms_mkstr(__LINE__)
+ *
+ * @param[in] x   expression to make string of
+ */
+#define AUDIO_COMMS_MK_ESCAPED_STR(x) #x
+#define AUDIO_COMMS_MKSTR(x) AUDIO_COMMS_MK_ESCAPED_STR(x)
 
+#include <stdio.h>
+#include <stdlib.h> /* assert */
+/**
+ * Checks a condition and abort if it is not met.
+ * This assert is ALWAYS activated (even in release mode), thus it must be used
+ * wisely.
+ *
+ * @param[in] cond  the condition to check
+ * @param[in] fmt   format of the error to display (printf like)
+ * @param[in] args  arguments of the error to display
+ */
+#define AUDIOCOMMS_ASSERT(cond, fmt, args...) \
+ do { \
+   if (audio_comms_unlikely(!(cond))) { \
+       fprintf(stderr, __BASE_FILE__ ":" audio_comms_mkstr(__LINE__) \
+               ": Assertion " #cond " failed: " fmt, ## args); \
+       fprintf(stderr, "\n"); \
+       abort(); \
+   } \
+ } while (0)
+
+#endif // __cplusplus
 
 #endif // AUDIOCOMMS_ASSERT_H
 
