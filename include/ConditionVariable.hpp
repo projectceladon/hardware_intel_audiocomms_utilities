5a72b684fe8e51e0df574ca587106825e349aa36
Thomas Cahuzac <thomasx.cahuzac@intel.com>
2013-12-09 14:57:30 +0100

    Utilities: adding a ConditionVariable class
    This patch adds an helper class to abstract the usage
    of pthread_cond_t tightly coupled with Mutex helper class.
    
    Change-Id: I2ae751e64f11bb56862c931a39579ebe6b28e534
    Tracked-On: GMINL-14461
    Signed-off-by: Thomas Cahuzac <thomasx.cahuzac@intel.com>
    Reviewed-on: http://android.intel.com:8080/150053
    Reviewed-by: Gonzalve, Sebastien <sebastien.gonzalve@intel.com>
    Reviewed-by: Soulard, Mathieu <mathieu.soulard@intel.com>
    Reviewed-on: 409548

---
+++ include/ConditionVariable.hpp
@@ -0,0 +1,95 @@
+/*
+ * @file
+ * Mutual exclusion class an utilities.
+ *
+ * @section License
+ *
+ * Copyright 2015 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "AudioCommsAssert.hpp"
+#include "Mutex.hpp"
+
+#include <pthread.h>
+#include <cerrno>
+#include <string.h>
+
+namespace audio_comms
+{
+namespace utilities
+{
+
+class ConditionVariable
+{
+public:
+    ConditionVariable()
+    {
+        int err = pthread_cond_init(&_cond, NULL);
+        AUDIOCOMMS_ASSERT(err == 0, "Unable to initialize condition variable @"
+                          << ": " << strerror(err) << "(" << err << ")");
+    }
+
+    ~ConditionVariable()
+    {
+        int err = pthread_cond_destroy(&_cond);
+        AUDIOCOMMS_ASSERT(err == 0, "Unable to destroy condition variable @"
+                          << static_cast<const void *>(&_cond)
+                          << ": " << strerror(err) << "(" << err << ")");
+    }
+
+    /**
+     * Unblocks at least one of the threads that are blocked on the specified condition variable
+     */
+    void signal()
+    {
+        int err = pthread_cond_signal(&_cond);
+        AUDIOCOMMS_ASSERT(err == 0, "Unable to signal condition variable @"
+                          << static_cast<const void *>(&_cond)
+                          << ": " << strerror(err) << "(" << err << ")");
+    }
+
+    /**
+     * Unblocks all threads that are blocked on the specified condition variable
+     */
+    void broadcast()
+    {
+        int err = pthread_cond_broadcast(&_cond);
+        AUDIOCOMMS_ASSERT(err == 0, "Unable to broadcast condition variable @"
+                          << static_cast<const void *>(&_cond)
+                          << ": " << strerror(err) << "(" << err << ")");
+    }
+
+    /**
+     * block the caller on a condition variable
+     *
+     * @param[in|out] _mutex The mutex that will be unlock when the waiting starts. It will be
+     *                       lock again the the waiting ends.
+     */
+    void wait(Mutex &mutex)
+    {
+        int err = pthread_cond_wait(&_cond, &mutex._mutex);
+        AUDIOCOMMS_ASSERT(err == 0, "Unable to wait for condition variable @"
+                          << static_cast<const void *>(&_cond)
+                          << ": " << strerror(err) << "(" << err << ")");
+    }
+
+private:
+    pthread_cond_t _cond; /**< internal condition variable*/
+};
+
+}
+}
