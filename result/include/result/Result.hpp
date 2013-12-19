e3bc8169faecca65b8d05a16059687e2f51251df
Kevin Rocard <kevinx.rocard@intel.com>
2013-12-19 14:30:28 +0100

    Move result to it's own lib
    BZ: 210780
    
    Result is cross lib as it aims to be used in API.
    
    This patch moves the result in its own lib.
    
    Change-Id: I5e1e1a261cefa9066f76ebdb37f2420c8a89842d
    Origin-Change-Id: I66614a7b2e62965253d3744c73847aca4685de44
    Signed-off-by: Kevin Rocard <kevinx.rocard@intel.com>
    Reviewed-on: http://android.intel.com:8080/152271

---
+++ result/include/result/Result.hpp
@@ -0,0 +1,191 @@
+/*
+ * INTEL CONFIDENTIAL
+ * Copyright (c) 2013 Intel
+ * Corporation All Rights Reserved.
+ *
+ * The source code contained or described herein and all documents related to
+ * the source code ("Material") are owned by Intel Corporation or its suppliers
+ * or licensors. Title to the Material remains with Intel Corporation or its
+ * suppliers and licensors. The Material contains trade secrets and proprietary
+ * and confidential information of Intel or its suppliers and licensors. The
+ * Material is protected by worldwide copyright and trade secret laws and
+ * treaty provisions. No part of the Material may be used, copied, reproduced,
+ * modified, published, uploaded, posted, transmitted, distributed, or
+ * disclosed in any way without Intel's prior express written permission.
+ *
+ * No license under any patent, copyright, trade secret or other intellectual
+ * property right is granted to or conferred upon you by disclosure or delivery
+ * of the Materials, either expressly, by implication, inducement, estoppel or
+ * otherwise. Any license under such intellectual property rights must be
+ * express and approved by Intel in writing.
+ */
+#pragma once
+#include <string>
+
+#include <sstream>
+
+namespace audio_comms
+{
+namespace cme
+{
+namespace result
+{
+
+/** A result that carries a error code describing the error.
+ *
+ * @tparam ErrorTrait a class describing a specific error field
+ *  ErrorTrait MUST have a Code type which hold its error values and MUST have
+ *  codeToString() static function that converts this code type into std::string.
+ *
+ * Can be cast to bool to see if the function succeed.
+ */
+template <class ErrorTrait>
+class Result
+{
+public:
+    /** Result constructor
+     *
+     * this creates a Result object which holds the given error code.
+     *
+     * @param[in] code the code to be put in Result; Default value is
+     *                       ErrorTrait::defaultError.
+     */
+    explicit Result(typename ErrorTrait::Code code = ErrorTrait::defaultError)
+        : _errorCode(code)
+    {}
+
+    /** Result constructor from an other result
+     *
+     * Construct a result where it's
+     *     error_code = inputResult.isFailure() ? failureCode : successCode
+     * If input result is a failure, append it (formated) to the message.
+     *
+     * @tparam InputTrait is the trait of the input result
+     * @param inputResult is the input result
+     * @param failureCode is the error code to use if the input result is a failure
+     * @param successCode is the error code to use if the input result is a success
+     */
+    template <class InputTrait>
+    explicit Result(const Result<InputTrait> &inputResult,
+                    typename ErrorTrait::Code failureCode,
+                    typename ErrorTrait::Code successCode = ErrorTrait::success)
+    {
+        if (inputResult.isFailure()) {
+            _errorCode = failureCode;
+            *this << inputResult.format();
+        } else {
+            _errorCode = successCode;
+        }
+    }
+
+    /** Get the error code.
+     *
+     * @return the error code, undefined behaviour if the result is a success.
+     */
+    typename ErrorTrait::Code getErrorCode() { return _errorCode; }
+
+    /** Get the error message
+     * @return the error message held by the class
+     */
+    const std::string &getMessage() const { return _message; }
+
+    /**
+     * Comparison operator.
+     * Careful, comparison is performed on error code only, messages MAY differ
+     * even if this function returns true.
+     *
+     * @param[in] toCompare  a Coderesult to compare with us
+     *
+     * @return true on equality, false otherwise
+     */
+    bool operator==(const Result<ErrorTrait> &toCompare) const
+    {
+        return _errorCode == toCompare._errorCode;
+    }
+
+    /**
+     * Success
+     * This function alway return a successful Result
+     *
+     * @return a successful coderesult
+     */
+    static const Result<ErrorTrait> &success()
+    {
+        static Result<ErrorTrait> success(ErrorTrait::success);
+        return success;
+    }
+
+    /**
+     * is object holding a Success value
+     *
+     * @return true is the error code is success, false otherwise
+     */
+    bool isSuccess() const { return _errorCode == ErrorTrait::success; }
+
+    /**
+     * Cast to ErrorTrait::Code
+     * This function allows to test a Result with an ErrorTrait::Code easily
+     *
+     * @return the code held by the object
+     */
+    operator typename ErrorTrait::Code() const {
+        return _errorCode;
+    }
+
+    /**
+     * is object holding a failure
+     *
+     * @return true if error code is NOT a success
+     */
+    bool isFailure() const { return !isSuccess(); }
+
+    /**
+     * Format a Result in oder to have it under a synthetic human readable view.
+     *
+     * @return a formated Result
+     */
+    std::string format() const
+    {
+        if (isFailure()) {
+            std::ostringstream ss;
+
+            // error code and error description
+            ss << "Code " << static_cast<int>(_errorCode) << ": " <<
+                ErrorTrait::codeToString(_errorCode);
+
+            // concatenate the message (if not empty)
+            if (_message.length() > 0) {
+                ss << " (" << _message << ")";
+            }
+            return ss.str();
+        }
+        return "Success";
+    }
+
+    /**
+     * Appends any data to message
+     * Careful: this function expects that T has a known conversion to ostream
+     *
+     * @tparam T   type of the thing to append
+     * @param[in]  aT  a T thing to append
+     *
+     * @return a copy of *this
+     */
+    template <class T>
+    Result<ErrorTrait> operator<<(const T &aT)
+    {
+        std::ostringstream ss;
+        ss << _message << aT;
+        _message = ss.str();
+        return *this;
+    }
+private:
+    typename ErrorTrait::Code _errorCode; /*< result code held by class */
+    std::string _message;                 /*< an error message explaining the error */
+};
+
+} /* namespace result */
+
+} /* namespace cme */
+
+} /* namespace audio_comms */
---
+++ result/test/ResultUnitTest.cpp
@@ -0,0 +1,178 @@
+/*
+ * INTEL CONFIDENTIAL
+ *
+ * Copyright 2013 Intel Corporation
+ * All Rights Reserved.
+ *
+ * The source code contained or described herein and all documents related
+ * to the source code ("Material") are owned by Intel Corporation or its
+ * suppliers or licensors.
+ * Title to the Material remains with Intel Corporation or its suppliers and
+ * licensors. The Material contains trade secrets and proprietary and
+ * confidential information of Intel or its suppliers and licensors.
+ * The Material is protected by worldwide copyright and trade secret laws and
+ * treaty provisions. No part of the Material may be used, copied, reproduced,
+ * modified, published, uploaded, posted, transmitted, distributed, or
+ * disclosed in any way without Intel's prior express written permission.
+ * No license under any patent, copyright, trade secret or other intellectual
+ * property right is granted to or conferred upon you by disclosure or delivery
+ * of the Materials, either expressly, by implication, inducement, estoppel or
+ * otherwise. Any license under such intellectual property rights must be
+ * express and approved by Intel in writing.
+ */
+
+#include "result/Result.hpp"
+
+#include <gtest/gtest.h>
+
+struct TestResult
+{
+    enum Code
+    {
+        A = 666,
+        B,
+        C,
+        D
+    };
+
+    static const Code success;
+    static const Code defaultError;
+
+    static std::string codeToString(const Code &code)
+    {
+        switch (code) {
+        case A:
+            return "A";
+        case B:
+            return "B";
+        case C:
+            return "C";
+        case D:
+            return "D";
+        }
+        /* FIXME ASSERT? */
+        return "";
+    }
+};
+
+const TestResult::Code TestResult::success = A;
+const TestResult::Code TestResult::defaultError = B;
+
+typedef audio_comms::cme::result::Result<TestResult> Result;
+
+TEST(Result, defaultValueConstructor)
+{
+    Result aResult;
+
+    /* Note, this also tests operator== */
+    EXPECT_EQ(Result(TestResult::defaultError), aResult);
+    EXPECT_EQ("", aResult.getMessage());
+}
+
+TEST(Result, someValueConstructor)
+{
+    Result aResult(TestResult::A);
+
+    EXPECT_EQ(Result(TestResult::A), aResult);
+    EXPECT_EQ("", aResult.getMessage());
+}
+
+TEST(Result, convertToCode)
+{
+    {
+        Result aResult(TestResult::success);
+        EXPECT_TRUE(aResult == TestResult::success);
+    }
+
+    {
+        Result aResult(TestResult::C);
+        EXPECT_FALSE(aResult.isSuccess());
+    }
+}
+
+TEST(Result, isFailure)
+{
+    {
+        Result aResult(TestResult::success);
+        EXPECT_FALSE(aResult.isFailure());
+        aResult << "that should not change success state";
+        EXPECT_FALSE(aResult.isFailure());
+    }
+
+    {
+        Result aResult(TestResult::D);
+        EXPECT_TRUE(aResult.isFailure());
+        aResult << "that should not change failure state";
+        EXPECT_TRUE(aResult.isFailure());
+    }
+}
+
+TEST(Result, isSuccess)
+{
+    {
+        Result aResult(TestResult::D);
+        EXPECT_FALSE(aResult.isSuccess());
+        aResult << "that should not change failure state";
+        EXPECT_FALSE(aResult.isSuccess());
+    }
+
+    {
+        Result aResult(TestResult::success);
+        EXPECT_TRUE(aResult.isSuccess());
+        aResult << "that should not change success state";
+        EXPECT_TRUE(aResult.isSuccess());
+    }
+}
+
+TEST(Result, equalityOperator)
+{
+    Result aResult(TestResult::success);
+    aResult << " Now error message differs.";
+    EXPECT_EQ(Result(TestResult::success),  aResult);
+}
+
+TEST(Result, getErrorCode)
+{
+    Result aResult(TestResult::success);
+    EXPECT_EQ(TestResult::success, aResult.getErrorCode());
+}
+
+TEST(Result, formatSuccess)
+{
+    Result aResult(TestResult::success);
+    EXPECT_EQ("Success", aResult.format());
+}
+
+TEST(Result, formatError)
+{
+    Result aResult(TestResult::C);
+    EXPECT_EQ("Code 668: C", aResult.format());
+}
+
+TEST(Result, useWithStream)
+{
+    Result aResult(TestResult::B);
+    aResult << "gnii";
+    EXPECT_EQ("gnii",  aResult.getMessage());
+    EXPECT_EQ("Code 667: B (gnii)",  aResult.format());
+}
+
+TEST(Result, successSingleton)
+{
+    EXPECT_EQ(Result::success(), Result(TestResult::success));
+}
+
+TEST(Result, constructFromfailureRes)
+{
+    Result bResult(TestResult::B);
+    bResult << "gnii";
+    Result trans(bResult, TestResult::D);
+    trans << "gnaa";
+    EXPECT_EQ("Code 669: D (Code 667: B (gnii)gnaa)",  trans.format());
+}
+
+TEST(Result, constructFromSuccessRes)
+{
+    Result trans(Result::success(), TestResult::D, TestResult::C);
+    EXPECT_EQ(TestResult::C, trans);
+}
