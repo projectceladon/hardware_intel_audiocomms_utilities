16410b7b2c23a77667f1fdb52c9d496a5cee42d6
Sebastien Gonzalve <sebastien.gonzalve@intel.com>
2014-06-27 15:08:35 +0200

    Remove dependency of XmlSerializer on ACME
    BZ: 212064
    
    In the legacy implementation, the root element and the version were hardcoded
    (because the original version of the code was written in the ACME project).
    
    This patch allows to specify the xml root element name and the version number.
    
    Change-Id: Iabb8d6d4e29666ad1b97f0d1287c7f71efc7e88c
    Reviewed-on: 212408
    Reviewed-by: Gonzalve, Sebastien <sebastien.gonzalve@intel.com>
    Tested-by: Gonzalve, Sebastien <sebastien.gonzalve@intel.com>

--- xmlserializer/include/xmlserializer/Serializer.hpp
+++ xmlserializer/include/xmlserializer/Serializer.hpp
@@ -24,6 +24,7 @@
 #include "serializer/framework/NamedTextTrait.hpp"
 #include "serializer/framework/Collection.hpp"
 #include <utilities/UniquePtr.hpp>
+#include <sstream>
 
 
 namespace audio_comms
@@ -33,19 +34,50 @@ namespace utilities
 namespace xmlserializer
 {
 
+/**
+ * XmlSerializer
+ * Class that handles xml serialization/deserialization
+ * The version numbering follows the Semantic Versioning scheme
+ */
 class XmlSerializer : public serializer::Serializer<Result>
 {
 public:
     typedef xmlserializer::Result Result;
 
+    /**
+     * Creates a XmlSerializer
+     * This class expect a document to be of the format:
+     *
+     * <?xml version="1.0" ?>
+     * <ROOT_ELEMENT_NAME version="1.2.3"> <! major is 1, minor 2, patch 3 >
+     * [... some xml data ]
+     * </ROOT_ELEMENT_NAME>
+     *
+     * @param[in] rootElementName  the name of the root element
+     * @param[in] major   the major number of the xml format version
+     * @param[in] minor   the minor number of the xml format version
+     * @param[in] patch   the patch number of the xml format version
+     */
+    XmlSerializer(const std::string &rootElementName,
+                  unsigned short major, unsigned short minor,
+                  unsigned short patch)
+        : mRootElementName(rootElementName)
+    {
+        std::ostringstream oss;
+        oss << major << "." << minor << "." << patch;
+        mVersion = oss.str();
+    }
+
     template <class Class>
-    static Result serialize(const Class &c, std::string &serializedCmd)
+    Result serialize(const Class &c, std::string &serializedCmd)
     {
         TiXmlOutStream stream;
         TiXmlDocument doc;
         doc.LinkEndChild(new TiXmlDeclaration("1.0", "", ""));
-        TiXmlElement root("ACME");
-        root.SetAttribute("version", "0.0.1");
+        TiXmlElement root(mRootElementName.c_str());
+
+        root.SetAttribute("version", mVersion.c_str());
+
         UniquePtr<TiXmlNode> xmlElement;
         Result res = XmlTraitSerializer<serializer::ClassSerializationTrait<Class> >
                      ::toXml(c, xmlElement.getRefToSet());
@@ -64,27 +96,55 @@ public:
         return Result::success();
     }
 
+    /**
+     * deserialize xml data contained in a string.
+     * If the parser is successful, Result::success is returned ans the class passed as reference
+     * is valid.
+     * In case an error is returned, the class is NOT valid.
+     * In case the version or the root element does not match, conversionFailed error is returned.
+     *
+     * @tparam Class   the class type to deserialize into
+     * @param[in] str  the string containing the xml document
+     * @param[out] c   the result class filled from the xml parsing
+     *
+     * @return a result describing the error
+     */
     template <class Class>
-    static Result deserialize(const std::string &str, Class &c)
+    Result deserialize(const std::string &str, Class &c)
     {
         TiXmlDocument doc;
         doc.Parse(str.c_str(), NULL);
 
-        const TiXmlNode *acmeTag = doc.FirstChildElement("ACME");
-        if (acmeTag == NULL) {
-            return Result(conversionFailed) << "No ACME root element.";
+        const TiXmlElement *rootTag = doc.FirstChildElement(mRootElementName.c_str());
+        if (rootTag == NULL) {
+            return Result(conversionFailed) << "No '" << mRootElementName.c_str()
+                                            << "' root element found in xml data.";
         }
 
-        /* Sanity check, there should NOT be any other node after de command's
-         * one */
-        if (acmeTag->NextSiblingElement() != NULL) {
+        const char *version = rootTag->Attribute("version");
+        if (version == NULL) {
+            return Result(conversionFailed) << "No version found in root node '"
+                                            << mRootElementName.c_str();
+        }
+
+        if (version != mVersion) {
+            return Result(conversionFailed) << "Version does not match; expect '" << mVersion
+                                            << "' got '" << version << "'.";
+        }
+
+        /* Sanity check, there should NOT be any other node after de command's one */
+        if (rootTag->NextSiblingElement() != NULL) {
             return Result(conversionFailed) << "No command tag found.";
         }
 
         return XmlTraitSerializer<serializer::ClassSerializationTrait<Class> >::fromXml(
-            *acmeTag->FirstChild(),
+            *rootTag->FirstChild(),
             c);
     }
+
+private:
+    std::string mRootElementName;
+    std::string mVersion;
 };
 
 } // namespace xmlserializer
