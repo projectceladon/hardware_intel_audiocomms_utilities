c4cea8f04aff4a0b3ddd7136e7e869a280a47c25
sgnanase <sundar.gnanasekaran@intel.com>
2017-07-04 12:21:11 +0530

    Resolve compilation issues for AIA on Android-O
    Jira : None
    Test : Device boots to homescreen
    
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- full_rw/Android.mk
+++ full_rw/Android.mk
@@ -13,13 +13,14 @@ LOCAL_C_INCLUDES += bionic
 include $(BUILD_STATIC_LIBRARY)
 
 # Build for host
-include $(CLEAR_VARS)
-LOCAL_COPY_HEADERS_TO := full_rw
-LOCAL_COPY_HEADERS :=  full_rw.h
-LOCAL_SRC_FILES := full_rw.c
-LOCAL_MODULE := libfull_rw_host
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Werror -Wall
-LOCAL_C_INCLUDES += bionic
-include $(BUILD_HOST_STATIC_LIBRARY)
+#include $(CLEAR_VARS)
+#LOCAL_COPY_HEADERS_TO := full_rw
+#LOCAL_COPY_HEADERS :=  full_rw.h
+#LOCAL_SRC_FILES := full_rw.c
+#LOCAL_MODULE := libfull_rw_host
+#LOCAL_MODULE_OWNER := intel
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_CFLAGS += -Werror -Wall
+#LOCAL_C_INCLUDES += bionic
+#include $(BUILD_STATIC_LIBRARY)
+#include $(BUILD_HOST_STATIC_LIBRARY)
